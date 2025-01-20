a2f6119a3da013ebd5744e4c09608ca0f4e7eff8
glycin <alex.chatzizacharias@gmail.com>
2025-01-20 23:08:28 +0100

    migrated to new plugin gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,58 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.2.0"
 }
 
+val v = "0.0.1"
+
 group = "com.glycin"
-version = "1.0-SNAPSHOT"
+version = v
 
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.2.6")
-    type.set("IC") // Target IDE Platform
+intellijPlatform  {
+    pluginConfiguration {
+        id = "intelliboom"
+        name = "IntelliBoom"
+        version = v
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+        ideaVersion {
+            sinceBuild = "232"
+            untilBuild = provider { null }
+        }
+
+        vendor {
+            name = "Glycin"
+            url = "https://github.com/glycin"
+        }
+    }
+
+
+    publishing {}
+
+    signing{}
+}
+
+dependencies {
+    intellijPlatform{
+        intellijIdeaCommunity("2024.2.3")
+        bundledPlugin("com.intellij.java")
+        pluginVerifier()
+        zipSigner()
+        testFramework(TestFrameworkType.Platform)
+    }
+
+    testImplementation("junit:junit:4.13.2")
 }
 
 tasks {
@@ -26,22 +61,10 @@ tasks {
         sourceCompatibility = "17"
         targetCompatibility = "17"
     }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
+}
 
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("242.*")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+kotlin{
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
